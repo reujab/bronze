7b38f5a455421ac1d258d0379bee2ce64c2e0e76
reujab <reujab@gmail.com>
2017-07-09 06:58:59 -0400

    use font awesome for home icon instead of an emoji

--- icons.go
+++ icons.go
@@ -1,14 +1,14 @@
 package main
 
 const (
-	iconSeparator     = "\ue0b0"     // Powerline
-	iconHome          = "\U0001f3e0" // Emoji
-	iconGithub        = "\uf09b"     // Font Awesome
-	iconGitlab        = "\uf296"     // Font Awesome
-	iconBitbucket     = "\uf171"     // Font Awesome
-	iconGit           = "\ue0a0"     // Powerline
-	iconStash         = "\uf01c"     // Font Awesome
-	iconAhead         = "\uf148"     // Font Awesome
-	iconModified      = "\u25cf"     // Unicode
-	iconIndexModified = "\u271a"     // Unicode
+	iconSeparator     = "\ue0b0" // Powerline
+	iconHome          = "\uf015" // Font Awesome
+	iconGithub        = "\uf09b" // Font Awesome
+	iconGitlab        = "\uf296" // Font Awesome
+	iconBitbucket     = "\uf171" // Font Awesome
+	iconGit           = "\ue0a0" // Powerline
+	iconStash         = "\uf01c" // Font Awesome
+	iconAhead         = "\uf148" // Font Awesome
+	iconModified      = "\u25cf" // Unicode
+	iconIndexModified = "\u271a" // Unicode
 )
