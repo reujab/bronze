6415b3f449ecb1c9336f4101393a416fb886baf1
reujab <reujab@gmail.com>
2017-07-09 05:49:47 -0400

    show git repo domain icon

--- git.go
+++ git.go
@@ -2,18 +2,23 @@ package main
 
 import (
 	"bufio"
+	"net/url"
 	"os/exec"
+	"strings"
 	"sync"
 )
 
 // the git segment provides useful information about a git repository such as the domain of the "origin" remote (with an icon), the current branch, and whether the HEAD is dirty
 func gitSegment(segment *segment) {
 	waitgroup := new(sync.WaitGroup)
-	waitgroup.Add(1)
+	waitgroup.Add(2)
 	var isRepo bool
+	var domain string
 	var modified, indexModified bool
 
 	go func() {
+		defer waitgroup.Done()
+
 		cmd := exec.Command("git", "status", "--porcelain")
 		stdout, err := cmd.StdoutPipe()
 		check(err)
@@ -33,18 +38,49 @@ func gitSegment(segment *segment) {
 
 		// the directory is in a git repo if the `git status` command exited successfully
 		isRepo = cmd.Wait() == nil
-		waitgroup.Done()
+	}()
+
+	go func() {
+		defer waitgroup.Done()
+
+		stdout, err := exec.Command("git", "config", "--get", "remote.origin.url").Output()
+		if err != nil {
+			return
+		}
+
+		uri, err := url.Parse(string(stdout))
+		check(err)
+		domain = uri.Hostname()
 	}()
 
 	waitgroup.Wait()
-	segment.visible = isRepo
+	if !isRepo {
+		return
+	}
+
+	var segments []string
+	switch domain {
+	case "github.com":
+		segments = append(segments, iconGithub)
+	case "gitlab.com":
+		segments = append(segments, iconGitlab)
+	case "bitbucket.com":
+		segments = append(segments, iconBitbucket)
+	default:
+		segments = append(segments, iconGit)
+	}
 	if modified || indexModified {
 		segment.background = "yellow"
+
+		var icons string
 		if modified {
-			segment.value += iconModified
+			icons += iconModified
 		}
 		if indexModified {
-			segment.value += iconIndexModified
+			icons += iconIndexModified
 		}
+		segments = append(segments, icons)
 	}
+	segment.visible = true
+	segment.value = strings.Join(segments, " ")
 }
--- icons.go
+++ icons.go
@@ -3,6 +3,10 @@ package main
 const (
 	iconSeparator     = "\ue0b0"     // Powerline
 	iconHome          = "\U0001f3e0" // Emoji
+	iconGithub        = "\uf09b"     // Font Awesome
+	iconGitlab        = "\uf296"     // Font Awesome
+	iconBitbucket     = "\uf171"     // Font Awesome
+	iconGit           = "\ue0a0"     // Powerline
 	iconModified      = "\u25cf"     // Unicode
 	iconIndexModified = "\u271a"     // Unicode
 )
