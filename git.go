d922e0d90832eb24532e2ae6482f3ced6ccedcdd
reujab <reujab@gmail.com>
2017-07-10 21:35:29 -0400

    started libgit2 recode

--- git.go
+++ git.go
@@ -1,134 +1,26 @@
 package main
 
 import (
-	"bufio"
-	"net/url"
 	"os"
-	"os/exec"
-	"path/filepath"
 	"strings"
-	"sync"
+
+	"github.com/libgit2/git2go"
 )
 
 // the git segment provides useful information about a git repository such as the domain of the "origin" remote (with an icon), the current branch, and whether the HEAD is dirty
 func gitSegment(segment *segment) {
-	// only run git commands if in a git repo
 	dir, err := os.Getwd()
 	check(err)
-	dir, err = filepath.EvalSymlinks(dir)
-	check(err)
-	var isRepo bool
-	for dir != "/" {
-		file, err := os.Stat(filepath.Join(dir, ".git"))
-		if err == nil && file.IsDir() {
-			isRepo = true
-			break
-		}
-		dir = filepath.Dir(dir)
-	}
-	if !isRepo {
+	repo, err := git.OpenRepositoryExtended(dir, 0, "/")
+	if err != nil {
 		return
 	}
 
-	waitgroup := new(sync.WaitGroup)
-	waitgroup.Add(5)
 	var domain string
 	var stashes int
 	var commitsAhead int
 	var branch string
 	var dirty, modified, staged bool
-
-	go func() {
-		defer waitgroup.Done()
-
-		cmd := exec.Command("git", "status", "--porcelain")
-		stdout, err := cmd.StdoutPipe()
-		check(err)
-		check(cmd.Start())
-
-		scanner := bufio.NewScanner(stdout)
-		for scanner.Scan() {
-			dirty = true
-
-			// https://git-scm.com/docs/git-status
-			x := scanner.Text()[0]
-			y := scanner.Text()[1]
-			if x == ' ' && (y == 'M' || y == 'D') {
-				modified = true
-			} else if x != '?' {
-				staged = true
-			}
-		}
-		check(scanner.Err())
-		check(cmd.Wait())
-	}()
-
-	go func() {
-		defer waitgroup.Done()
-
-		cmd := exec.Command("git", "stash", "list")
-		stdout, err := cmd.StdoutPipe()
-		check(err)
-		check(cmd.Start())
-
-		scanner := bufio.NewScanner(stdout)
-		for scanner.Scan() {
-			stashes++
-		}
-		check(scanner.Err())
-
-		check(cmd.Wait())
-	}()
-
-	go func() {
-		defer waitgroup.Done()
-
-		cmd := exec.Command("git", "log", "--oneline", "@{upstream}..")
-		stdout, err := cmd.StdoutPipe()
-		check(err)
-		check(cmd.Start())
-
-		scanner := bufio.NewScanner(stdout)
-		for scanner.Scan() {
-			commitsAhead++
-		}
-		check(scanner.Err())
-
-		cmd.Wait()
-	}()
-
-	go func() {
-		defer waitgroup.Done()
-
-		stdout, err := exec.Command("git", "rev-parse", "--abbrev-ref", "HEAD").Output()
-		if err != nil {
-			// no commits yet
-			return
-		}
-		branch = strings.TrimSpace(string(stdout))
-
-		// if the head is detached, display the short hash
-		if branch == "HEAD" {
-			stdout, err = exec.Command("git", "rev-parse", "--short", "HEAD").Output()
-			check(err)
-			branch = strings.TrimSpace(string(stdout))
-		}
-	}()
-
-	go func() {
-		defer waitgroup.Done()
-
-		stdout, err := exec.Command("git", "config", "--get", "remote.origin.url").Output()
-		if err != nil {
-			// no configured remote
-			return
-		}
-		uri, err := url.Parse(string(stdout))
-		check(err)
-		domain = uri.Hostname()
-	}()
-
-	waitgroup.Wait()
 
 	var segments []string
 	switch domain {
