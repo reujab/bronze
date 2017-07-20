a74feb7e8c37b4c420e47cada2625f0fe080431f
reujab <reujab@gmail.com>
2017-07-20 15:18:06 -0400

    added BRONZE_DIR_ALIASES

--- dir.go
+++ dir.go
@@ -3,6 +3,7 @@ package main
 import (
 	"os"
 	"os/user"
+	"path/filepath"
 	"strconv"
 	"strings"
 
@@ -10,16 +11,30 @@ import (
 )
 
 func dirSegment(segment *Segment) {
+	var aliases []string
+	if os.Getenv("BRONZE_DIR_ALIASES") != "" {
+		aliases = strings.Split(os.Getenv("BRONZE_DIR_ALIASES"), ":")
+	}
+	if len(aliases)%2 != 0 {
+		dief("dir: invalid BRONZE_DIR_ALIASES")
+	}
+
 	usr, err := user.Current()
 	die(err)
 	segment.Value, err = os.Getwd()
 	die(err)
-	length, _ := strconv.Atoi(os.Getenv("BRONZE_DIR_LENGTH"))
+	aliases = append(aliases, usr.HomeDir, icons["home"])
 
-	if strings.HasPrefix(segment.Value, usr.HomeDir) {
-		segment.Value = icons["home"] + segment.Value[len(usr.HomeDir):]
+	for i := 0; i < len(aliases); i += 2 {
+		prefix := filepath.Clean(aliases[i])
+		icon := aliases[i+1]
+		if strings.HasPrefix(segment.Value, prefix) {
+			segment.Value = icon + segment.Value[len(prefix):]
+			break
+		}
 	}
 
+	length, _ := strconv.Atoi(os.Getenv("BRONZE_DIR_LENGTH"))
 	if length != 0 {
 		dirs := strings.Split(segment.Value, string(os.PathSeparator))
 		for i := len(dirs) - 2; i >= 0; i-- {
