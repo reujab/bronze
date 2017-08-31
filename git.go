01b4cf1c377db9a7978dc279c09739624094a150
reujab <reujab@gmail.com>
2017-08-31 09:39:22 -0400

    workaround libgit2 v26 issue

--- git.go
+++ git.go
@@ -5,8 +5,8 @@ import (
 	"os"
 	"strings"
 
-	"github.com/libgit2/git2go"
 	. "github.com/reujab/bronze/types"
+	"github.com/reujab/git2go-v26"
 )
 
 // the git segment provides useful information about a git repository such as the domain of the "origin" remote (with an icon), the current branch, and whether the HEAD is dirty
